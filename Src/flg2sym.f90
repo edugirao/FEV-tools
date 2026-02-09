787c111adc4dcdd38b095e8844bbec4b717be166
Eduardo Girao <edugirao@gmail.com>
2026-02-09 08:25:34 -0300

    Rename program and update file reading logic

--- Src/flg2sym.f90
+++ Src/flg2sym.f90
@@ -1,50 +1,29 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-PROGRAM flg_to_symmetry                                                      !!!
+PROGRAM flg2sym                                                              !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+USE tools_maps                                                               !!!
+USE tools_read                                                               !!!
+USE tools_symm                                                               !!!
+USE tools_writ                                                               !!!
 IMPLICIT NONE                                                                !!!
-INTEGER:: nf,nflags,nrotmaps,nmirmaps,nglimaps,ntramaps                      !!!
-INTEGER:: mirror_count,i                                                     !!!
+INTEGER:: nf,ne,nv,nflags,nrotmaps,nmirmaps,nglimaps,ntramaps                !!!
+INTEGER:: mirror_count,nmaps,nrot,nmir                                       !!!
 INTEGER,ALLOCATABLE:: flag(:,:),nface(:),m2(:,:),m1(:,:)                     !!!
 INTEGER,ALLOCATABLE:: flag_color(:),neigh_flag(:,:)                          !!!
+INTEGER,ALLOCATABLE:: maps(:,:),maps_nfixed(:)                               !!!
+INTEGER,ALLOCATABLE:: rot_i(:),rot_n(:),mir_i(:),mir_m(:)                    !!!
+CHARACTER*1,ALLOCATABLE:: rot_e(:),mir_e(:)                                  !!!
 CHARACTER*100:: filename                                                     !!!
 CHARACTER*4:: group                                                          !!!
-LOGICAL:: have_file_b,have_file_f                                            !!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! Reading                                                                    !!!
+LOGICAL:: nocross                                                            !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! Identifying input file                                                     !!!
-CALL read_init(filename)                                                     !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! Reading flag graph and faces sizes                                         !!!
+CALL read_init(filename,'inp')                                               !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-INQUIRE(FILE=TRIM(ADJUSTL(filename))//'.b.flg',EXIST=have_file_b)            !!!
-IF(have_file_b)THEN                                                          !!!
-  OPEN(UNIT=3,FILE=TRIM(ADJUSTL(filename))//'.b.flg',FORM='UNFORMATTED')     !!!
-  READ(3) nflags,nf                                                          !!!
-  ALLOCATE(flag(nflags,3),neigh_flag(nflags,3),flag_color(nflags),nface(nf)) !!!
-  READ(3) nface                                                              !!!
-  READ(3) flag                                                               !!!
-  READ(3) neigh_flag                                                         !!!
-  READ(3) flag_color                                                         !!!
-  CLOSE(UNIT=3)                                                              !!!
-ELSE                                                                         !!!
-  INQUIRE(FILE=TRIM(ADJUSTL(filename))//'.flg',EXIST=have_file_f)            !!!
-  IF(have_file_f)THEN                                                        !!!
-    OPEN(UNIT=3,FILE=TRIM(ADJUSTL(filename))//'.flg')                        !!!
-    READ(3,*) nflags,nf                                                      !!!
-    ALLOCATE(flag(nflags,3),neigh_flag(nflags,3))                            !!!
-    ALLOCATE(flag_color(nflags),nface(nf))                                   !!!
-    READ(3,*) nface                                                          !!!
-    DO i=1,nflags                                                            !!!
-      READ(3,*) flag(i,:),neigh_flag(i,:),flag_color(i)                      !!!
-    END DO                                                                   !!!
-    CLOSE(UNIT=3)                                                            !!!
-  ELSE                                                                       !!!
-    STOP 'Error: no .flg or .b.flg file found'                               !!!
-  END IF                                                                     !!!
-END IF                                                                       !!!
-                                                                             !!!
-                                                                             !!!
+! Reading the flag graph from a .flg or a .b.flg file                        !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+CALL read_flg(nf,ne,nv,nflags,nface,flag,neigh_flag,flag_color,filename)     !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! Neighbors matrices (fev_flag.f90)                                          !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -57,28 +36,36 @@ CALL second_neighbors(nflags,flag,m2,m1,flag_color)                          !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! Creating maps (fev_maps.f90)                                               !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! Searching for the maps                                                     !!!
-CALL creating_maps(nflags,flag,neigh_flag,m1,m2,nf,nface,filename)           !!!
+! Searching maps                                                             !!!
+CALL creating_maps(nflags,flag,neigh_flag,m1,m2,nf,nface,nmaps,maps,maps_nfixed)
+! Writing maps                                                               !!!
+CALL write_map(nflags,nmaps,maps,maps_nfixed,filename)                       !!!
 ! Identifying map types                                                      !!!
-CALL map_types(nflags,flag_color,nrotmaps,nmirmaps,nglimaps,ntramaps,filename)
+CALL map_types(nflags,flag_color,nrotmaps,nmirmaps,nglimaps,ntramaps,nmaps,maps,maps_nfixed)
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! Extracting symmetries from maps (fev_symm.f90)                             !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-CALL get_axes(nflags,flag,m1,filename)                                       !!!
-CALL get_mirror(nflags,flag,m2,filename)                                     !!!
-CALL counting_mirrors(filename,nflags,flag,m2,mirror_count)                  !!!
+CALL get_rot(nflags,flag,m1,nmaps,maps,maps_nfixed,nrot,rot_e,rot_i,rot_n)   !!!
+CALL write_rot(nrot,rot_e,rot_i,rot_n,filename)                              !!!
+CALL get_mir(nflags,flag,m2,nmaps,maps,maps_nfixed,nmir,mir_e,mir_i,mir_m)   !!!
+CALL write_mir(nmir,mir_e,mir_i,mir_m,filename)                              !!!
+CALL counting_mirrors(nflags,flag,m2,nmaps,maps,maps_nfixed,mirror_count)    !!!
+IF(((nrotmaps.eq.1).AND.(nmirmaps.eq.2).AND.(nglimaps.eq.0)).OR. &           !!!
+ & ((nrotmaps.eq.2).AND.(nmirmaps.eq.3).AND.(nglimaps.eq.0)))THEN            !!!
+  ! Cheking for axis off-mirrors                                             !!!
+  CALL axis_off_mirrors(nrot,rot_i,rot_e,nmir,mir_i,mir_e,nocross)           !!!
+END IF                                                                       !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! Identifying wallpaper group                                                !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-CALL symmetry_group(nrotmaps,nmirmaps,nglimaps,filename,mirror_count,group)  !!!
+CALL symmetry_group(nrotmaps,nmirmaps,nglimaps,mirror_count,nocross,group)   !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! Deallocations                                                              !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 DEALLOCATE(flag,neigh_flag,flag_color,nface,m1,m2)                           !!!
-! WRITE(*,'(A)') group                                                       !!!
 OPEN(UNIT=1,FILE=TRIM(ADJUSTL(filename))//'.sym')                            !!!
 WRITE(1,'(A)') group                                                         !!!
 CLOSE(UNIT=1)                                                                !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-END PROGRAM flg_to_symmetry                                                  !!!
+END PROGRAM flg2sym                                                          !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
