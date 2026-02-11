47e3b2d5b259b0ec4889e9d8e86ae0c086489aa5
Eduardo Girao <edugirao@gmail.com>
2026-02-11 14:20:21 -0300

    Refactor fcs2xyz.f90 to improve variable management

--- Src/fcs2xyz.f90
+++ Src/fcs2xyz.f90
@@ -5,11 +5,14 @@ USE tools_coor                                                               !!!
 USE tools_read                                                               !!!
 USE tools_writ                                                               !!!
 IMPLICIT NONE                                                                !!!
-INTEGER:: i,nf,i0,f0,ie1,ie2,nv1,nf1                                         !!!
-INTEGER,ALLOCATABLE:: nface1(:),x_in_f1(:,:),y_in_f1(:,:)                    !!!
+INTEGER:: i,nf,i0,f0,ie1,ie2,nf0,ne0,nv0,nmax0,nf1,ne1,nv1,nmax1             !!!
+INTEGER,ALLOCATABLE:: nface0(:),f_in_f0(:,:),e_in_f0(:,:),v_in_f0(:,:)       !!!
+INTEGER,ALLOCATABLE:: x_in_f0(:,:),y_in_f0(:,:)                              !!!
+INTEGER,ALLOCATABLE:: nface1(:),f_in_f1(:,:),e_in_f1(:,:),v_in_f1(:,:)       !!!
+INTEGER,ALLOCATABLE:: x_in_f1(:,:),y_in_f1(:,:)                              !!!
 CHARACTER*100:: filename_target,filename_p                                   !!!
 CHARACTER*100,ALLOCATABLE:: filename_list(:)                                 !!!
-REAL(KIND=8),ALLOCATABLE:: r(:,:)                                            !!!
+REAL(KIND=8),ALLOCATABLE:: rp(:,:),rk(:,:)                                   !!!
 LOGICAL:: have_file                                                          !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! Getting target information                                                 !!!
@@ -41,13 +44,26 @@ DO i=i0,nf                                                                   !!!
   print*, 'Making ',filename_list(i)                                         !!!
   ! Getting ancestor info                                                    !!!
   CALL read_anc(filename_list(i),filename_p,f0,ie1,ie2)                      !!!
+  ! Reading parent .fcs (allocations in read_fcs_only_fev_in_f)              !!!
+  CALL read_fcs_only_fev_in_f(nf0,ne0,nv0,nmax0,nface0,f_in_f0,e_in_f0,v_in_f0,filename_p)
+  ! Reading child .fcs (allocations in read_fcs_only_fev_in_f)               !!!
+  CALL read_fcs_only_fev_in_f(nf1,ne1,nv1,nmax1,nface1,f_in_f1,e_in_f1,v_in_f1,filename_list(i))
+  ! Reading parent .nxy (allocations inside)                                 !!!
+  CALL read_nxy(nf0,nmax0,nface0,x_in_f0,y_in_f0,filename_p)                 !!!
+  ! Reading parent structure                                                 !!!
+  CALL read_xydata(nv0,rp,filename_p)                                        !!!  
   ! Optimizing the xyz                                                       !!!
-  CALL make_a_xyz(filename_list(i),filename_p,f0,ie1,ie2,nv1,r,nf1,nface1,x_in_f1,y_in_f1)
+  CALL add_a_dimer(f0,ie1,ie2,rp, &                                          !!!
+          & nf0,nv0,nmax0,nface0,f_in_f0,e_in_f0,v_in_f0,x_in_f0,y_in_f0, &  !!!
+          & nf1,nv1,nmax1,nface1,x_in_f1,y_in_f1,rk)                         !!!
+  ! Optimizing child structure                                               !!!
+  CALL xyz_optimization(nf1,nv1,nmax1,nface1,v_in_f1,x_in_f1,y_in_f1,rk,filename_list(i))  
   !  Writing the xyz                                                         !!!
-  CALL write_xyz(nv1,r,filename_list(i))                                     !!!
+  CALL write_xyz(nv1,rk,filename_list(i))                                    !!!
   !  Writing the nxy                                                         !!!
   CALL write_nxy(nf1,MAXVAL(nface1),nface1,x_in_f1,y_in_f1,filename_list(i)) !!!
-  DEALLOCATE(r,x_in_f1,y_in_f1,nface1)                                       !!!
+  DEALLOCATE(rp,f_in_f0,e_in_f0,v_in_f0,nface0,x_in_f0,y_in_f0)              !!!
+  DEALLOCATE(rk,f_in_f1,e_in_f1,v_in_f1,nface1,x_in_f1,y_in_f1)              !!!
 END DO                                                                       !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 END PROGRAM fcs2xyz                                                          !!!
