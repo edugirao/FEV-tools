fff1dc737d6f9d33fa677aa24793aebe347ca161
Eduardo Girao <edugirao@gmail.com>
2026-02-11 14:21:58 -0300

    Refactor fev2sum to write sum rules instead of flags
    Updated the program to include writing sum rules and removed the previous flag creation logic.

--- Src/fev2sum.f90
+++ Src/fev2sum.f90
@@ -1,12 +1,12 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 PROGRAM fev2sum                                                              !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-USE tools_flag                                                               !!!
 USE tools_read                                                               !!!
 USE tools_rule                                                               !!!
+USE tools_writ                                                               !!!
 IMPLICIT NONE                                                                !!!
-INTEGER:: i,j,k,nf,ne,nv,nflags                                              !!!
-INTEGER,ALLOCATABLE:: flag(:,:),nface(:),fev(:,:,:)                          !!!
+INTEGER:: nf,ne,nv                                                           !!!
+INTEGER,ALLOCATABLE:: nface(:),fev(:,:,:)                                    !!!
 INTEGER,ALLOCATABLE:: srule_fe(:),srule_fv(:),srule_ev(:)                    !!!
 CHARACTER*100:: filename                                                     !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -18,47 +18,18 @@ CALL read_init(filename,'inp')                                               !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 CALL read_fev(nf,ne,nv,fev,filename)                                         !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Faces sizes (allocations inside)                                           !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+CALL nfaces_from_fev(nf,ne,nv,fev,nface)                                     !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! Computing sum rules                                                        !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ALLOCATE(srule_fe(nv),srule_fv(ne),srule_ev(nf))                             !!!
 CALL sum_rules(nf,ne,nv,fev,srule_fe,srule_fv,srule_ev)                      !!!
-OPEN(UNIT=1,FILE=TRIM(ADJUSTL(filename))//'.sum')                            !!!
-WRITE(1,'(A)') '!****************** Sum Rules *******************!'          !!!
-WRITE(1,'(A,I0)') 'Euler Characteristic -> ',nf-ne+nv                        !!!
-WRITE(1,'(A)') '!------------------------------------------------!'          !!!
-WRITE(1,'(A)') 'Face-Edge rule test'                                         !!!
-DO k=1,nv                                                                    !!!
-  WRITE(1,'(A,I3,A,I2)')  'Vertex',k,'->',srule_fe(k)                        !!!
-END DO                                                                       !!!
-WRITE(1,'(A)') '!------------------------------------------------!'          !!!
-WRITE(1,'(A)') 'Face-Vertex rule test'                                       !!!
-DO j=1,ne                                                                    !!!
-  WRITE(1,'(A,I3,A,I2)') 'Edge',j,'->',srule_fv(j)                           !!!
-END DO                                                                       !!!
-WRITE(1,'(A)') '!------------------------------------------------!'          !!!
-WRITE(1,'(A)') 'Edge-Vertex rule test'                                       !!!
-DO i=1,nf                                                                    !!!
-  WRITE(1,'(A,I3,A,I2)')  'Face',i,'->',srule_ev(i)                          !!!
-END DO                                                                       !!!
-WRITE(1,'(A)') '!************************************************!'          !!!
-WRITE(1,'(A)') '! Faces Sizes                                    !'          !!!
-WRITE(1,'(A)') '!------------------------------------------------!'          !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! Creating flags (fev_flag.f90)                                              !!!
+! Writing sum rules                                                          !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! Building flags                                                             !!!
-nflags=6*nv ! We have 6 flags for each vertex                                !!!
-ALLOCATE(flag(nflags,3))                                                     !!!
-flag=0                                                                       !!!
-CALL build_flags(nflags,flag,nf,ne,nv,fev)                                   !!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! Faces sizes                                                                !!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-ALLOCATE(nface(nf))                                                          !!!
-CALL faces_sizes(nf,nflags,flag,nface)                                       !!!
-WRITE(1,'(100I7)') nface                                                     !!!
-WRITE(1,'(A)') '!************************************************!'          !!!
-CLOSE(UNIT=1)                                                                !!!
+CALL write_sum_rules(nf,ne,nv,srule_fe,srule_fv,srule_ev,nface,filename)     !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 END PROGRAM fev2sum                                                          !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
