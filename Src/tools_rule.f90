13412f1b496fe6d529789544253b0848bed3fbbf
Eduardo Girao <edugirao@gmail.com>
2026-02-11 16:19:23 -0300

    Refactor subroutines for face and edge rules

--- Src/tools_rule.f90
+++ Src/tools_rule.f90
@@ -1,8 +1,10 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 MODULE tools_rule                                                            !!!
 IMPLICIT NONE                                                                !!!
-PUBLIC                                                                       !!!
+PUBLIC ! maybe make some private in the future                               !!!
 CONTAINS                                                                     !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 SUBROUTINE sum_rules(nf,ne,nv,fev,srule_fe,srule_fv,srule_ev)                !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -29,23 +31,71 @@ END SUBROUTINE sum_rules                                                     !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-SUBROUTINE faces_sizes(nf,nflags,flag,nface)                                 !!!
+SUBROUTINE nfaces_from_fev(nf,ne,nv,fev,nface)                               !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! Routine to apply the FE, FV, and EV sum rules.                             !!!
 ! Also gets number of sides of each face.                                    !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 IMPLICIT NONE                                                                !!!
-INTEGER:: i,nf,nflags,flag(nflags,3),nface(nf)                               !!!
+INTEGER,ALLOCATABLE,INTENT(OUT):: nface(:)                                   !!!
+INTEGER,INTENT(IN):: nf,ne,nv,fev(nf,ne,nv)                                  !!!
+INTEGER:: srule_ev(nf),srule_fv(ne),reduc_fe(nf,ne),i,j                      !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! Counting flags for each face                                               !!!
-nface=0                                                                      !!!
-DO i=1,nflags                                                                !!!
-  nface(flag(i,1))=nface(flag(i,1))+1                                        !!!
+! Edge-Vertex rule                                                           !!!
+CALL sum_rules_ev(nf,ne,nv,fev,srule_ev)                                     !!!
+! Provisional faces sizes                                                    !!!
+ALLOCATE(nface(nf))                                                          !!!
+nface=srule_ev/2                                                             !!!
+! Face-Vertex rule                                                           !!!
+CALL sum_rules_fv(nf,ne,nv,fev,srule_fv)                                     !!!
+! Face-Edge matrix reduction                                                 !!!
+CALL matrix_reduction_fe(nf,ne,nv,fev,reduc_fe)                              !!!
+! Correcting faces sizes to account for bridges                              !!!
+DO i=1,nf                                                                    !!!
+  DO j=1,ne                                                                  !!!
+    ! Edge within the face                                                   !!!
+    IF(reduc_fe(i,j).ne.0)THEN                                               !!!
+      ! Add one more side to the face if we got a bridge                     !!!
+      IF(srule_fv(j).eq.2) nface(i)=nface(i)+1                               !!!
+    END IF                                                                   !!!
+  END DO                                                                     !!!
 END DO                                                                       !!!
-! Removing double counting                                                   !!!
-nface=nface/2                                                                !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-END SUBROUTINE faces_sizes                                                   !!!
+END SUBROUTINE nfaces_from_fev                                               !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+SUBROUTINE sum_rules_ev(nf,ne,nv,fev,srule_ev)                               !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Routine to apply the EV sum rule.                                          !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+IMPLICIT NONE                                                                !!!
+INTEGER:: i,nf,ne,nv,fev(nf,ne,nv)                                           !!!
+INTEGER:: srule_ev(nf)                                                       !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Edge-Vertex rule                                                           !!!
+DO i=1,nf                                                                    !!!
+  srule_ev(i)=SUM(fev(i,:,:))                                                !!!
+END DO                                                                       !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+END SUBROUTINE sum_rules_ev                                                  !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+SUBROUTINE sum_rules_fv(nf,ne,nv,fev,srule_fv)                               !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Routine to apply the FV sum rule.                                          !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+IMPLICIT NONE                                                                !!!
+INTEGER:: j,nf,ne,nv,fev(nf,ne,nv)                                           !!!
+INTEGER:: srule_fv(ne)                                                       !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Face-Vertex rule                                                           !!!
+DO j=1,ne                                                                    !!!
+  srule_fv(j)=SUM(fev(:,j,:))                                                !!!
+END DO                                                                       !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+END SUBROUTINE sum_rules_fv                                                  !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -65,6 +115,25 @@ END DO                                                                       !!!
 END DO                                                                       !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 END SUBROUTINE matrix_reduction_ev                                           !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+SUBROUTINE matrix_reduction_fe(nf,ne,nv,fev,reduc_fe)                        !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Routine to apply the EV matrix reduction.                                  !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+IMPLICIT NONE                                                                !!!
+INTEGER:: i,j,nf,ne,nv,fev(nf,ne,nv),reduc_fe(nf,ne)                         !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Face-Edge reduction                                                        !!!
+reduc_fe=0                                                                   !!!
+DO i=1,nf                                                                    !!!
+DO j=1,ne                                                                    !!!
+  reduc_fe(i,j)=SUM(fev(i,j,:))                                              !!!
+END DO                                                                       !!!
+END DO                                                                       !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+END SUBROUTINE matrix_reduction_fe                                           !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
